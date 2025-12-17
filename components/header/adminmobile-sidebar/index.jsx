0ad2cbae9f6bbecdbf9c1cd68bda9fa4304dfa49
krativeweb <krativewebdeveloper@gmail.com>
2025-12-17 22:45:06 +0530

    update

--- components/header/adminmobile-sidebar/index.jsx
+++ components/header/adminmobile-sidebar/index.jsx
@@ -2,7 +2,6 @@
 
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import mobileMenuData from "../../../data/adminMobilemenudata";
-/* import SidebarFooter from "./SidebarFooter"; */
 import SidebarHeader from "./SidebarHeader";
 import {
   isActiveLink,
@@ -10,7 +9,7 @@ import {
 } from "../../../utils/linkActiveChecker";
 import { usePathname, useRouter } from "next/navigation";
 
-const Index = () => {
+const MobileMenu = () => {
   const router = useRouter();
   const pathname = usePathname();
 
@@ -21,68 +20,76 @@ const Index = () => {
       id="offcanvasMenu"
       data-bs-scroll="true"
     >
+      {/* ================= HEADER ================= */}
       <SidebarHeader />
+
       <p
         style={{
           textAlign: "center",
-          fontWeight: "800",
+          fontWeight: 800,
           fontSize: "20px",
           marginTop: "20px",
-          backgroundColor: "#043389", // Set background color
-          color: "white", // Set text color
-          padding: "10px", // Optional: add some padding
-          borderRadius: "5px", // Optional: rounded corners
+          backgroundColor: "#043389",
+          color: "#fff",
+          padding: "10px",
+          borderRadius: "5px",
         }}
       >
         Admin Panel
       </p>
+
+      {/* ================= SIDEBAR ================= */}
       <Sidebar>
         <Menu>
           {mobileMenuData.map((item) =>
-            item.items && item.items.length > 0 ? (
+            item.children && item.children.length > 0 ? (
               <SubMenu
+                key={item.id}
+                label={item.name}
+                icon={
+                  item.icon ? <i className={`la ${item.icon}`} /> : null
+                }
                 className={
-                  isActiveParentChaild(item.items, pathname)
+                  isActiveParentChaild(item.children, pathname)
                     ? "menu-active"
                     : ""
                 }
-                label={item.label}
-                key={item.id}
               >
-                {item.items.map((menuItem, i) => (
+                {item.children.map((child) => (
                   <MenuItem
-                    onClick={() => router.push(menuItem.routePath)}
+                    key={child.id}
+                    onClick={() => router.push(child.routePath)}
                     className={
-                      isActiveLink(menuItem.routePath, pathname)
+                      isActiveLink(child.routePath, pathname)
                         ? "menu-active-link"
                         : ""
                     }
-                    key={i}
                   >
-                    {menuItem.label}
+                    {child.name}
                   </MenuItem>
                 ))}
               </SubMenu>
             ) : (
               <MenuItem
+                key={item.id}
+                icon={
+                  item.icon ? <i className={`la ${item.icon}`} /> : null
+                }
                 onClick={() => router.push(item.routePath)}
                 className={
                   isActiveLink(item.routePath, pathname)
                     ? "menu-active-link"
                     : ""
                 }
-                key={item.id}
               >
-                {item.label}
+                {item.name}
               </MenuItem>
             )
           )}
         </Menu>
       </Sidebar>
-
-      {/*  <SidebarFooter /> */}
     </div>
   );
 };
 
-export default Index;
+export default MobileMenu;
--- data/adminMobilemenudata.js
+++ data/adminMobilemenudata.js
@@ -1,45 +1,150 @@
 module.exports = [
   {
     id: 1,
-    label: "Dashboard",
+    name: "Dashboard Admin",
+    icon: "la-home",
     routePath: "/admin/dashboard",
+    active: "active",
   },
+
   {
     id: 2,
-    label: "List Company",
-    routePath: "/admin/listcompany",
+    name: "Mandatory Disclosure",
+    icon: "la-building",
+    routePath: "/admin/mandatory-disclosure",
+    active: "",
   },
-  // {
-  //   id: 3,
-  //   label: "List Company Self",
-  //   routePath: "/admin/listcompanyself",
-  // },
 
   {
     id: 3,
-    label: "List Institute",
-    routePath: "/admin/listinstitute",
+    name: "About IMT Hyderabad",
+    icon: "la-wallet",
+    routePath: "/admin/about-imt-hyderabad",
+    active: "",
   },
 
   {
     id: 4,
-    label: "List Candidate",
-    routePath: "/admin/listcandidate",
+    name: "International Relation",
+    icon: "la-wallet",
+    routePath: "/admin/international-relations",
+    active: "",
   },
 
   {
     id: 5,
-    label: "List Packages",
-    routePath: "/admin/listpackage",
+    name: "Manage Others",
+    icon: "la-id-badge",
+    routePath: "/admin/others",
+    active: "",
   },
-  // {
-  //   id: 5,
-  //   label: "List Verified Candidate",
-  //   routePath: "/admin/listverified",
-  // },
-  // {
-  //   id: 6,
-  //   label: "List Verified Employee",
-  //   routePath: "/list-verified-employee",
-  // },
+
+  {
+    id: 6,
+    name: "Manage Placement",
+    icon: "la-boxes",
+    routePath: "/admin/placement",
+    active: "",
+  },
+
+  {
+    id: 7,
+    name: "Manage Faculty",
+    icon: "la-user-tie",
+    children: [
+      {
+        id: "7-1",
+        name: "Faculty Page Seo Settings",
+        routePath: "/admin/faculty-seo-settings",
+      },
+      {
+        id: "7-2",
+        name: "List Faculty",
+        routePath: "/admin/list-faculty",
+      },
+      {
+        id: "7-3",
+        name: "Faculty Departments",
+        routePath: "/admin/faculty/departments",
+      },
+    ],
+  },
+
+  {
+    id: 8,
+    name: "Manage LRC",
+    icon: "la-book",
+    children: [
+      {
+        id: "8-1",
+        name: "LRC Page Details",
+        icon: "la-file-signature",
+        routePath: "/admin/lrc-page-details",
+      },
+      {
+        id: "8-2",
+        name: "List LRC FAQs",
+        icon: "la-question-circle",
+        routePath: "/admin/lrc-faqs",
+      },
+    ],
+  },
+
+  {
+    id: 9,
+    name: "Manage Awards & Recognitions",
+    icon: "la-book",
+    children: [
+      {
+        id: "9-1",
+        name: "Page Seo Settings",
+        icon: "la-file-signature",
+        routePath: "/admin/award-seo-settings",
+      },
+      {
+        id: "9-2",
+        name: "Institute Awards",
+        icon: "la-question-circle",
+        routePath: "/admin/institute-awards",
+      },
+      {
+        id: "9-3",
+        name: "Faculty Awards",
+        icon: "la-question-circle",
+        routePath: "/admin/faculty-awards",
+      },
+    ],
+  },
+
+  {
+    id: 10,
+    name: "Manage Home Settings",
+    icon: "la-book",
+    children: [
+      {
+        id: "10-1",
+        name: "Page Seo Settings",
+        icon: "la-file-signature",
+        routePath: "/admin/home-settings",
+      },
+      {
+        id: "10-2",
+        name: "About Us Settings",
+        icon: "la-question-circle",
+        routePath: "/admin/about-us",
+      },
+      {
+        id: "10-3",
+        name: "Manage USP Section",
+        icon: "la-question-circle",
+        routePath: "/admin/usp-section",
+      },
+      {
+        id: "10-4",
+        name: "Manage Research Section",
+        icon: "la-question-circle",
+        routePath: "/admin/research-infocus",
+      },
+    ],
+  },
 ];
