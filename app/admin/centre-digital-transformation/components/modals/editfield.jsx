c9e864e2e43000929e376388e002c23717b0aaed
krativeweb <krativewebdeveloper@gmail.com>
2026-01-28 11:24:47 +0530

    Update editfield.jsx

--- app/admin/centre-digital-transformation/components/modals/editfield.jsx
+++ app/admin/centre-digital-transformation/components/modals/editfield.jsx
@@ -114,7 +114,7 @@ const EditfieldModal = ({ show, onClose, field, onSave }) => {
                   type="text"
                   className="form-control"
                   value={formData.page_title}
-                  readOnly
+                  onChange={handleChange}
                 />
               </div>
 
