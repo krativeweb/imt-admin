ce3d1d9587bf003d6b1c3b8ade8e0e0e10bb4459
krativeweb <krativewebdeveloper@gmail.com>
2026-01-30 12:31:57 +0530

    Update InstituteMenuRight.jsx

--- data/InstituteMenuRight.jsx
+++ data/InstituteMenuRight.jsx
@@ -96,7 +96,7 @@ module.exports = [
   //   icon: "la-lock",
   //   routePath: "/employers-dashboard/change-password",
   //   active: "",
-  // },
+  // }, 
   // {
   //   id: 15,
   //   name: "Logout",
