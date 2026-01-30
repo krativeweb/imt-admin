e718a11eb413b0fae0a7eabdff2c4cbcd42598fd
krativeweb <krativewebdeveloper@gmail.com>
2026-01-30 12:45:03 +0530

    Update About.jsx

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (   
     <>
       <div className="content-column col-lg-6 col-md-12 col-sm-12 order-2">
-        <div className="inner-column " data-aos="fade-left"> 
+        <div className="inner-column " data-aos="fade-left">  
           <div className="sec-title">
             <h2>Millions of Jobs. Find the one that suits you.</h2> 
             <div className="text"> 
